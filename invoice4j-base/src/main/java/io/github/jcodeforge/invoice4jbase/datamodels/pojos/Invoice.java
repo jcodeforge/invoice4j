40d6e6ff0b279841fad126016389a8ead8e35c5a
al.scholz <a.scholz@fuhrpark-software.de>
2026-08-03 15:34:30 +0200

    Move method to DefaultInvoiceValidator

--- invoice4j-base/src/main/java/io/github/jcodeforge/invoice4jbase/datamodels/pojos/Invoice.java
+++ invoice4j-base/src/main/java/io/github/jcodeforge/invoice4jbase/datamodels/pojos/Invoice.java
@@ -691,48 +691,6 @@ public final class Invoice {
             return this;
         }
 
-        private void validateDocumentTotals() {
-            Objects.requireNonNull(monetarySummation);
-
-            BigDecimal lineExtensionTotal = lines.stream()
-                    .map(line -> line.getLineExtensionAmount().getAmount())
-                    .reduce(BigDecimal.ZERO, BigDecimal::add);
-
-            BigDecimal allowanceTotal = allowanceCharges.stream()
-                    .filter(ac -> !ac.isCharge())
-                    .map(AllowanceCharge::getAmount)
-                    .filter(Objects::nonNull)
-                    .map(MonetaryAmount::getAmount)
-                    .reduce(BigDecimal.ZERO, BigDecimal::add);
-
-            BigDecimal chargeTotal = allowanceCharges.stream()
-                    .filter(AllowanceCharge::isCharge)
-                    .map(AllowanceCharge::getAmount)
-                    .filter(Objects::nonNull)
-                    .map(MonetaryAmount::getAmount)
-                    .reduce(BigDecimal.ZERO, BigDecimal::add);
-
-            BigDecimal taxExclusiveAmount = lineExtensionTotal
-                    .subtract(allowanceTotal)
-                    .add(chargeTotal);
-
-            if (lineExtensionTotal.compareTo(monetarySummation.getLineExtensionAmount().getAmount()) != 0) {
-                throw new InvoiceValidationException("BT-106 Line extension amount mismatch.");
-            }
-
-            if (allowanceTotal.compareTo(monetarySummation.getAllowanceTotalAmount().getAmount()) != 0) {
-                throw new InvoiceValidationException("BT-107 Allowance total amount mismatch.");
-            }
-
-            if (chargeTotal.compareTo(monetarySummation.getChargeTotalAmount().getAmount()) != 0) {
-                throw new InvoiceValidationException("BT-108 Charge total amount mismatch.");
-            }
-
-            if (taxExclusiveAmount.compareTo(monetarySummation.getTaxExclusiveAmount().getAmount()) != 0) {
-                throw new InvoiceValidationException("BT-109 Tax exclusive amount mismatch.");
-            }
-        }
-
         public Builder from(Invoice invoice) {
             if (invoice == null) {
                 throw new InvoiceValidationException("Invoice must not be null.");
@@ -850,8 +808,6 @@ public final class Invoice {
                     throw new InvoiceValidationException("BT-117 VAT amount mismatch.");
                 }
             }
-
-            validateDocumentTotals();
 
             return new Invoice(this);
         }
