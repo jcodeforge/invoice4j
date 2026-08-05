7b5642856f3f7d911e54afbb52f5b96516cab96a
al.scholz <a.scholz@fuhrpark-software.de>
2026-08-05 15:22:18 +0200

    Add SerializationException

---
+++ invoice4j-base/src/main/java/io/github/jcodeforge/invoice4jbase/exceptions/SerializationException.java
@@ -0,0 +1,8 @@
+package io.github.jcodeforge.invoice4jbase.exceptions;
+
+public class SerializationException extends RuntimeException {
+
+    public SerializationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
