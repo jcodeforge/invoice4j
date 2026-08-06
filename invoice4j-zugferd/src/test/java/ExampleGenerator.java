59fd5a00186c7782838b07c9849731d83b866021
al.scholz <a.scholz@fuhrpark-software.de>
2026-08-06 11:06:22 +0200

    Add ExampleGenerator

---
+++ invoice4j-zugferd/src/test/java/ExampleGenerator.java
@@ -0,0 +1,15 @@
+import factory.TestInvoiceFactory;
+import io.github.jcodeforge.invoice4jzugferd.CiiInvoiceWriter;
+import java.io.File;
+
+public final class ExampleGenerator {
+
+    public static void main(String[] args) {
+        CiiInvoiceWriter writer = CiiInvoiceWriter.builder()
+                .prettyPrint(true)
+                .build();
+
+        writer.writeToFile(TestInvoiceFactory.createMinimalInvoice(), new File("examples/minimal-invoice.xml"));
+        writer.writeToFile(TestInvoiceFactory.createCompleteInvoice(), new File("examples/complete-invoice.xml"));
+    }
+}
