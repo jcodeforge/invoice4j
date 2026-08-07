ad6df0eeb30494387b24c190088c139f0c1b3588
al.scholz <a.scholz@fuhrpark-software.de>
2026-08-07 13:28:30 +0200

    Add XsdValidationException

---
+++ invoice4j-base/src/main/java/io/github/jcodeforge/invoice4jbase/exceptions/XsdValidationException.java
@@ -0,0 +1,8 @@
+package io.github.jcodeforge.invoice4jbase.exceptions;
+
+public class XsdValidationException extends RuntimeException {
+
+    public XsdValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
