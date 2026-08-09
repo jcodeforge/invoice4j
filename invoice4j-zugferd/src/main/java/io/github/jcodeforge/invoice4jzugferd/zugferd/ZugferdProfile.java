57519c9087e3cfec09ea28d14a353e5fc5b49f47
al.scholz <a.scholz@fuhrpark-software.de>
2026-08-09 10:25:55 +0200

    Add ZugferdInvoiceWriter

---
+++ invoice4j-zugferd/src/main/java/io/github/jcodeforge/invoice4jzugferd/zugferd/ZugferdProfile.java
@@ -0,0 +1,29 @@
+package io.github.jcodeforge.invoice4jzugferd.zugferd;
+
+public enum ZugferdProfile {
+
+    /**
+     * ZUGFeRD MINIMUM profile.
+     */
+    MINIMUM,
+
+    /**
+     * ZUGFeRD BASIC-WL profile.
+     */
+    BASIC_WL,
+
+    /**
+     * ZUGFeRD BASIC profile.
+     */
+    BASIC,
+
+    /**
+     * ZUGFeRD EN 16931 / COMFORT profile.
+     */
+    EN16931,
+
+    /**
+     * ZUGFeRD EXTENDED profile.
+     */
+    EXTENDED
+}
