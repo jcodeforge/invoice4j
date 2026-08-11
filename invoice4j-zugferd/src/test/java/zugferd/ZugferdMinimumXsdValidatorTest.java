2dea666c13e8246805b74ff3a822c45cce65b825
al.scholz <a.scholz@fuhrpark-software.de>
2026-08-11 19:40:38 +0200

    Add ZugferdMinimumXsdValidator

---
+++ invoice4j-zugferd/src/test/java/zugferd/ZugferdMinimumXsdValidatorTest.java
@@ -0,0 +1,45 @@
+package zugferd;
+
+import factory.TestInvoiceFactory;
+import io.github.jcodeforge.invoice4jbase.calculation.InvoiceCalculator;
+import io.github.jcodeforge.invoice4jbase.datamodels.pojos.Invoice;
+import io.github.jcodeforge.invoice4jbase.exceptions.XsdValidationException;
+import io.github.jcodeforge.invoice4jzugferd.zugferd.ZugferdInvoiceWriter;
+import io.github.jcodeforge.invoice4jzugferd.zugferd.ZugferdProfile;
+import io.github.jcodeforge.invoice4jzugferd.zugferd.validation.ZugferdMinimumXsdValidator;
+import org.junit.Test;
+
+public class ZugferdMinimumXsdValidatorTest {
+
+    private final ZugferdMinimumXsdValidator SUT = new ZugferdMinimumXsdValidator();
+
+    @Test
+    public void shouldValidateMinimumInvoice() {
+        Invoice invoice = new InvoiceCalculator().calculate(TestInvoiceFactory.createMinimalInvoice());
+
+        String xml = ZugferdInvoiceWriter.builder()
+                .profile(ZugferdProfile.MINIMUM)
+                .build()
+                .writeToString(invoice);
+
+        SUT.validate(xml);
+    }
+
+    @Test(expected = XsdValidationException.class)
+    public void shouldRejectInvalidMinimumXml() {
+        String invalidXml = "...";
+        SUT.validate(invalidXml);
+    }
+
+    @Test
+    public void shouldValidateCompleteMinimumInvoice() {
+        Invoice invoice = new InvoiceCalculator().calculate(TestInvoiceFactory.createCompleteInvoice());
+
+        String xml = ZugferdInvoiceWriter.builder()
+                .profile(ZugferdProfile.MINIMUM)
+                .build()
+                .writeToString(invoice);
+
+        SUT.validate(xml);
+    }
+}
