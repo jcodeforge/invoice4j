1b814fb2d8a23e44045fec34d40551cdd4d7667d
al.scholz <a.scholz@fuhrpark-software.de>
2026-08-11 19:42:04 +0200

    Support zugferd MINIMUM profile

--- invoice4j-zugferd/src/main/java/io/github/jcodeforge/invoice4jzugferd/zugferd/ZugferdInvoiceWriter.java
+++ invoice4j-zugferd/src/main/java/io/github/jcodeforge/invoice4jzugferd/zugferd/ZugferdInvoiceWriter.java
@@ -71,7 +71,7 @@ public final class ZugferdInvoiceWriter {
             case EN16931 -> CiiProfile.ZUGFERD_EN16931;
             case BASIC -> CiiProfile.ZUGFERD_BASIC;
             case BASIC_WL -> throw new UnsupportedOperationException("ZUGFeRD BASIC-WL profile is not implemented yet.");
-            case MINIMUM -> throw new UnsupportedOperationException("ZUGFeRD MINIMUM profile is not implemented yet.");
+            case MINIMUM -> CiiProfile.ZUGFERD_MINIMUM;
             case EXTENDED -> throw new UnsupportedOperationException("ZUGFeRD EXTENDED profile is not implemented yet.");
         };
     }
