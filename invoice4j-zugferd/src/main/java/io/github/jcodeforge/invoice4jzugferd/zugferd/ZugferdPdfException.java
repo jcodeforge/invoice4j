94fc5845021e5d4062971bc16bfc5bc000b42029
al.scholz <a.scholz@fuhrpark-software.de>
2026-08-12 15:24:51 +0200

    Add ZugferdPdfException

---
+++ invoice4j-zugferd/src/main/java/io/github/jcodeforge/invoice4jzugferd/zugferd/ZugferdPdfException.java
@@ -0,0 +1,12 @@
+package io.github.jcodeforge.invoice4jzugferd.zugferd;
+
+public class ZugferdPdfException extends RuntimeException {
+
+    public ZugferdPdfException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ZugferdPdfException(String message) {
+        super(message);
+    }
+}
