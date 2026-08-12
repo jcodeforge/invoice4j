12e0bd671c742bb23261010ba6697da01bdf8bc1
al.scholz <a.scholz@fuhrpark-software.de>
2026-08-12 13:26:03 +0200

    Refactor

--- invoice4j-zugferd/src/main/java/io/github/jcodeforge/invoice4jzugferd/xml/XmlReaderFactory.java
+++ invoice4j-zugferd/src/main/java/io/github/jcodeforge/invoice4jzugferd/xml/XmlReaderFactory.java
@@ -1,7 +1,9 @@
 package io.github.jcodeforge.invoice4jzugferd.xml;
 
 import io.github.jcodeforge.invoice4jbase.exceptions.DeserializationException;
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
@@ -14,17 +16,7 @@ public final class XmlReaderFactory {
         Objects.requireNonNull(inputStream, "inputStream must not be null");
 
         try {
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-
-            factory.setNamespaceAware(true);
-
-            // Secure XML processing
-            factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-            factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
-            factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
-            factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
-            factory.setXIncludeAware(false);
-            factory.setExpandEntityReferences(false);
+            DocumentBuilderFactory factory = createFactory();
 
             DocumentBuilder builder = factory.newDocumentBuilder();
             Document document = builder.parse(inputStream);
@@ -34,5 +26,26 @@ public final class XmlReaderFactory {
         } catch (Exception e) {
             throw new DeserializationException("Unable to create XML reader.", e);
         }
+    }
+
+    public static XmlReader create(String xml) {
+        Objects.requireNonNull(xml, "xml must not be null");
+        return create(new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8)));
+    }
+
+    private static DocumentBuilderFactory createFactory() throws Exception {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+        factory.setNamespaceAware(true);
+
+        // Secure XML processing
+        factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+        factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+        factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+        factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+        factory.setXIncludeAware(false);
+        factory.setExpandEntityReferences(false);
+
+        return factory;
     }
 }
