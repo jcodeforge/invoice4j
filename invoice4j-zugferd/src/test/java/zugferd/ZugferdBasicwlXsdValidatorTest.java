2482cae954036a945869139c102a84f45fcd0f4b
al.scholz <a.scholz@fuhrpark-software.de>
2026-08-12 12:06:26 +0200

    Add ZugferdBasicwlXsdValidator and unit test

---
+++ invoice4j-zugferd/src/test/java/zugferd/ZugferdBasicwlXsdValidatorTest.java
@@ -0,0 +1,47 @@
+package zugferd;
+
+import factory.TestInvoiceFactory;
+import io.github.jcodeforge.invoice4jbase.calculation.InvoiceCalculator;
+import io.github.jcodeforge.invoice4jbase.datamodels.pojos.Invoice;
+import io.github.jcodeforge.invoice4jbase.exceptions.XsdValidationException;
+import io.github.jcodeforge.invoice4jzugferd.zugferd.ZugferdInvoiceWriter;
+import io.github.jcodeforge.invoice4jzugferd.zugferd.ZugferdProfile;
+import io.github.jcodeforge.invoice4jzugferd.zugferd.validation.ZugferdBasicwlXsdValidator;
+import org.junit.Test;
+
+public class ZugferdBasicwlXsdValidatorTest {
+
+    private final ZugferdBasicwlXsdValidator SUT = new ZugferdBasicwlXsdValidator();
+
+    @Test
+    public void shouldValidateBasicwlInvoice() {
+        Invoice invoice = new InvoiceCalculator().calculate(TestInvoiceFactory.createMinimalInvoice());
+
+        String xml = ZugferdInvoiceWriter.builder()
+                .profile(ZugferdProfile.BASIC_WL)
+                .build()
+                .writeToString(invoice);
+
+        System.out.println(xml);
+
+        SUT.validate(xml);
+    }
+
+    @Test(expected = XsdValidationException.class)
+    public void shouldRejectInvalidBasicwlXml() {
+        String invalidXml = "...";
+        SUT.validate(invalidXml);
+    }
+
+    @Test
+    public void shouldValidateCompleteBasicwlInvoice() {
+        Invoice invoice = new InvoiceCalculator().calculate(TestInvoiceFactory.createCompleteInvoice());
+
+        String xml = ZugferdInvoiceWriter.builder()
+                .profile(ZugferdProfile.BASIC_WL)
+                .build()
+                .writeToString(invoice);
+
+        SUT.validate(xml);
+    }
+}
