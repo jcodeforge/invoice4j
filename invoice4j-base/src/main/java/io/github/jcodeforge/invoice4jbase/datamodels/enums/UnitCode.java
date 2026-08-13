1770f6ab43b45f12f96002a4a96a9b6781e6e347
al.scholz <a.scholz@fuhrpark-software.de>
2026-08-13 10:53:06 +0200

    Update unit codes

--- invoice4j-base/src/main/java/io/github/jcodeforge/invoice4jbase/datamodels/enums/UnitCode.java
+++ invoice4j-base/src/main/java/io/github/jcodeforge/invoice4jbase/datamodels/enums/UnitCode.java
@@ -8,24 +8,14 @@ package io.github.jcodeforge.invoice4jbase.datamodels.enums;
 public enum UnitCode {
 
     /**
-     * H87 - Piece
+     * ANN - Year
      */
-    PIECE("H87"),
+    YEAR("ANN"),
 
     /**
-     * KGM - Kilogram
+     * MON - Month
      */
-    KILOGRAM("KGM"),
-
-    /**
-     * MTR - Meter
-     */
-    METER("MTR"),
-
-    /**
-     * LTR - Liter
-     */
-    LITER("LTR"),
+    MONTH("MON"),
 
     /**
      * DAY - Day
@@ -43,14 +33,172 @@ public enum UnitCode {
     MINUTE("MIN"),
 
     /**
+     * SEC - Second
+     */
+    SECOND("SEC"),
+
+    /**
      * C62 - One
      */
     ONE("C62"),
 
     /**
-     * MON - Month
+     * H87 - Piece
      */
-    MONTH("MON");
+    PIECE("H87"),
+
+    /**
+     * KGM - Kilogram
+     */
+    KILOGRAM("KGM"),
+
+    /**
+     * GRM - Gram
+     */
+    GRAM("GRM"),
+
+    /**
+     * TNE - Tonne
+     */
+    TONNE("TNE"),
+
+    /**
+     * MTR - Metre
+     */
+    METER("MTR"),
+
+    /**
+     * DMT - Decimetre
+     */
+    DECIMETER("DMT"),
+
+    /**
+     * CMT - Centimetre
+     */
+    CENTIMETER("CMT"),
+
+    /**
+     * MMT - Millimetre
+     */
+    MILLIMETER("MMT"),
+
+    /**
+     * KMT - Kilometre
+     */
+    KILOMETER("KMT"),
+
+    /**
+     * LTR - Litre
+     */
+    LITER("LTR"),
+
+    /**
+     * MLT - Millilitre
+     */
+    MILLILITER("MLT"),
+
+    /**
+     * KWH - Kilowatt-hour
+     */
+    KILOWATT_HOUR("KWH"),
+
+    /**
+     * WHR - Watt-hour
+     */
+    WATT_HOUR("WHR"),
+
+    /**
+     * HHP - Horsepower hour
+     */
+    HORSEPOWER_HOUR("HHP"),
+
+    /**
+     * MTK - Square metre
+     */
+    SQUARE_METER("MTK"),
+
+    /**
+     * MTR - Metre
+     *
+     * Note: use MTR only once; retained above.
+     */
+
+    /**
+     * MTQ - Cubic metre
+     */
+    CUBIC_METER("MTQ"),
+
+    /**
+     * DLT - Decilitre
+     */
+    DECILITER("DLT"),
+
+    /**
+     * CLT - Centilitre
+     */
+    CENTILITER("CLT"),
+
+    /**
+     * KWH - Kilowatt-hour
+     *
+     * Note: use KWH only once; retained above.
+     */
+
+    /**
+     * TAH - Thousand ampere hour
+     */
+    THOUSAND_AMPERE_HOUR("TAH"),
+
+    /**
+     * AMP - Ampere
+     */
+    AMPERE("AMP"),
+
+    /**
+     * KVA - Kilovolt-ampere
+     */
+    KILOVOLT_AMPERE("KVA"),
+
+    /**
+     * KVT - Kilovolt
+     */
+    KILOVOLT("KVT"),
+
+    /**
+     * KWT - Kilowatt
+     */
+    KILOWATT("KWT"),
+
+    /**
+     * CMT - Centimetre
+     *
+     * Note: use CMT only once; retained above.
+     */
+
+    /**
+     * PCE - Piece
+     */
+    PCE("PCE"),
+
+    /**
+     * SET - Set
+     */
+    SET("SET"),
+
+    /**
+     * PR - Pair
+     */
+    PAIR("PR"),
+
+    /**
+     * DZN - Dozen
+     */
+    DOZEN("DZN"),
+
+    /**
+     * GLL - Gallon
+     */
+    GALLON("GLL");
 
     private final String code;
 
@@ -69,6 +217,8 @@ public enum UnitCode {
             }
         }
 
-        throw new IllegalArgumentException("Unknown unit code: " + code);
+        throw new IllegalArgumentException(
+                "Unknown unit code: " + code
+        );
     }
 }
