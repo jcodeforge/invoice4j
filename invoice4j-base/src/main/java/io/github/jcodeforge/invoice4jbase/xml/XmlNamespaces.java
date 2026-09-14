fcd5d13135872db73f87becd9f6df59ea13245d8
al.scholz <a.scholz@fuhrpark-software.de>
2026-09-14 14:48:56 +0200

    Provide ubl namespaces

--- invoice4j-base/src/main/java/io/github/jcodeforge/invoice4jbase/xml/XmlNamespaces.java
+++ invoice4j-base/src/main/java/io/github/jcodeforge/invoice4jbase/xml/XmlNamespaces.java
@@ -2,16 +2,20 @@ package io.github.jcodeforge.invoice4jbase.xml;
 
 public final class XmlNamespaces {
 
-    public static final String RSM =
-            "urn:un:unece:uncefact:data:standard:CrossIndustryInvoice:100";
+    private XmlNamespaces() {
+    }
 
-    public static final String RAM =
-            "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100";
+    public static final String RSM = "urn:un:unece:uncefact:data:standard:CrossIndustryInvoice:100";
 
-    public static final String QDT =
-            "urn:un:unece:uncefact:data:standard:QualifiedDataType:100";
+    public static final String RAM = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100";
 
-    public static final String UDT =
-            "urn:un:unece:uncefact:data:standard:UnqualifiedDataType:100";
+    public static final String QDT = "urn:un:unece:uncefact:data:standard:QualifiedDataType:100";
 
+    public static final String UDT = "urn:un:unece:uncefact:data:standard:UnqualifiedDataType:100";
+
+    public static final String UBL_INVOICE = "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2";
+
+    public static final String UBL_CAC = "urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2";
+
+    public static final String UBL_CBC = "urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2";
 }
