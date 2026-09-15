63c0c1f65fa9e66b17ee8066f6bb0574ef1624aa
al.scholz <a.scholz@fuhrpark-software.de>
2026-09-15 12:07:08 +0200

    Add KositValidatorUblTest

---
+++ invoice4j-xr/src/test/java/validation/KositValidatorUblTest.java
@@ -0,0 +1,59 @@
+package validation;
+
+import io.github.jcodeforge.invoice4jbase.calculation.InvoiceCalculator;
+import io.github.jcodeforge.invoice4jbase.datamodels.pojos.Invoice;
+import io.github.jcodeforge.invoice4jbase.testfactory.TestInvoiceFactory;
+import io.github.jcodeforge.invoice4jxr.XrUblInvoiceWriter;
+import io.github.jcodeforge.invoice4jxr.validation.KositValidator;
+import io.github.jcodeforge.invoice4jxr.validation.ValidationResult;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class KositValidatorUblTest {
+
+    private final KositValidator SUT = new KositValidator();
+
+    @Test
+    public void shouldValidateXRechnungUbl() throws Exception {
+        Invoice invoice = new InvoiceCalculator().calculate(TestInvoiceFactory.createCompleteInvoice());
+
+        String xml = XrUblInvoiceWriter.builder()
+                .validate(false)
+                .prettyPrint(true)
+                .build()
+                .writeToString(invoice);
+
+        ValidationResult result = SUT.validate(xml);
+
+        assertNotNull(result);
+    }
+
+    @Test
+    public void shouldAcceptValidXRechnungUbl() {
+        Invoice invoice = new InvoiceCalculator().calculate(TestInvoiceFactory.createCompleteInvoice());
+
+        String xml = XrUblInvoiceWriter.builder()
+                .validate(false)
+                .build()
+                .writeToString(invoice);
+
+        ValidationResult result = SUT.validate(xml);
+
+        System.out.println("valid = " + result.isValid());
+
+        result.getMessages().forEach(message ->
+                System.out.println(
+                        message.getSeverity()
+                                + ": "
+                                + message.getMessage()
+                                + " | rule="
+                                + message.getRuleId()
+                                + " | location="
+                                + message.getLocation()
+                )
+        );
+
+        assertTrue(result.isValid());
+    }
+}
